043276b1de0e0a7471de8261a2e499be720816bd
Chris Kibble <chris@ckib.me>
2021-12-20 15:42:13 -0500

    Changed version to variable and used variable type instead of string

--- check_lmcontroller_service.ps1
+++ check_lmcontroller_service.ps1
@@ -1,16 +1,20 @@
-$check_service = get-service "ImControllerservice" -ea silentlycontinue
+
+$patchedVersion = [Version]"1.1.20.3"
+
+$check_service = Get-Service "ImControllerservice" -ea silentlycontinue
+
 If($check_service -ne $null)
 	{
 		$Controller_Path = "C:\Windows\Lenovo\ImController\PluginHost\Lenovo.Modern.ImController.PluginHost.exe"
 		If(test-path $Controller_Path)
 			{
-				$Get_Version = (get-item $Controller_Path | select *).VersionInfo.ProductVersion
-				If($Get_Version -lt "1.1.20.3")
+				[Version]$Get_Version = (get-item $Controller_Path | select *).VersionInfo.ProductVersion
+				If($Get_Version -lt $patchedVersion)
 					{
 						write-output "Service installed and bad version ($Get_Version)"	  
 						EXIT 1					
 					}
-				ElseIf($Get_Version -ge "1.1.20.3")
+				ElseIf($Get_Version -ge $patchedVersion)
 					{
 						write-output "Service installed but version OK ($Get_Version)"	  
 						EXIT 0				
