b47c421842abb40bf49b1b0a7223aa24399eb2dd
Chris Kibble <chris@ckib.me>
2021-12-20 15:45:39 -0500

    Changed Get-Service to Using WMI so we could use the dynamic path name.

--- check_lmcontroller_service.ps1
+++ check_lmcontroller_service.ps1
@@ -1,25 +1,21 @@
 
 $patchedVersion = [Version]"1.1.20.3"
 
-$check_service = Get-Service "ImControllerservice" -ea silentlycontinue
+$check_service = Get-CimInstance Win32_Service -Filter "Name = 'ImControllerservice'" -Property PathName
 
 If($check_service -ne $null)
 	{
-		$Controller_Path = "C:\Windows\Lenovo\ImController\PluginHost\Lenovo.Modern.ImController.PluginHost.exe"
-		If(test-path $Controller_Path)
+		[Version]$Get_Version = (get-item $Controller_Path | select *).VersionInfo.ProductVersion
+		If($Get_Version -lt $patchedVersion)
 			{
-				[Version]$Get_Version = (get-item $Controller_Path | select *).VersionInfo.ProductVersion
-				If($Get_Version -lt $patchedVersion)
-					{
-						write-output "Service installed and bad version ($Get_Version)"	  
-						EXIT 1					
-					}
-				ElseIf($Get_Version -ge $patchedVersion)
-					{
-						write-output "Service installed but version OK ($Get_Version)"	  
-						EXIT 0				
-					}					
+				write-output "Service installed and bad version ($Get_Version)"	  
+				EXIT 1					
 			}
+		ElseIf($Get_Version -ge $patchedVersion)
+			{
+				write-output "Service installed but version OK ($Get_Version)"	  
+				EXIT 0				
+			}					
 	}
 Else
 	{
